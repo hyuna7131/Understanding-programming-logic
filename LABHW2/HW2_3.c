9ca7db711a886ce68f0c66c51933397aaf8ba620
hyuna7131 <105485238+hyuna7131@users.noreply.github.com>
2022-11-13 08:23:23 -0800

    Add files via upload

---
+++ LABHW2/HW2_3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void)
+{
+	int √—√ , Ω√, ∫–, √ ;
+	√—√  = 14000;
+	Ω√ = √—√  / 3600;
+	∫– = √—√  % 3600 / 60;
+	√  = √—√  % 36000 % 60;
+	printf("%d seconds:\n", √—√ );
+	printf("%dh %dm %ds\n", Ω√, ∫–, √ );
+}
