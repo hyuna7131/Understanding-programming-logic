70b2dd9f6ee10b46ea1b3269c7b91de2fb209362
hyunal7131 <hyunal7131@gmail.com>
2022-11-14 02:21:12 +0900

    Understanding-programming-logic

--- HW1_1/HW1_1/HW1_1.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main(void)
-{
-		printf("Photoshop\tFun\n");
-		printf("Java Script\tEasy\n");
-		printf("Java\t\tPowerful\n");
-		printf("C++\t\tInteresting\n");
-		printf("C\t\tVery Important\n");
-}
